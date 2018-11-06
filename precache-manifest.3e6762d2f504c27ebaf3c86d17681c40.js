532b9d4f6be245894d60fd0094a3e2b87cd0ea4c
Yudi Kichi <yudi_kichi@hotmail.com>
2018-11-06 14:09:18 -0200

    Updates

---
+++ precache-manifest.3e6762d2f504c27ebaf3c86d17681c40.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a73be096bb948d7990bf56b36b13f61",
+    "url": "/web-site/static/media/photo.5a73be09.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a9ccc868bada396dd2db753a3cef9501",
+    "url": "/web-site/static/media/curriculo.a9ccc868.pdf"
+  },
+  {
+    "revision": "9936d6e80ea0980d098a06d649b48f68",
+    "url": "/web-site/static/media/android.9936d6e8.jpg"
+  },
+  {
+    "revision": "69d2768f05e9e472b557d17a2231449b",
+    "url": "/web-site/static/media/abrindoportas.69d2768f.png"
+  },
+  {
+    "revision": "c21b83f11af5cc6ada97",
+    "url": "/web-site/static/js/runtime~main.c21b83f1.js"
+  },
+  {
+    "revision": "6c3e65904d22979919cf",
+    "url": "/web-site/static/js/main.6c3e6590.chunk.js"
+  },
+  {
+    "revision": "7ee5bb0c702e7d1b5aaf",
+    "url": "/web-site/static/js/1.7ee5bb0c.chunk.js"
+  },
+  {
+    "revision": "6c3e65904d22979919cf",
+    "url": "/web-site/static/css/main.d21115fc.chunk.css"
+  },
+  {
+    "revision": "7a810555a15a5cb6c5f4850103f42cf7",
+    "url": "/web-site/index.html"
+  }
+];
